fd0de851c52351f2fbfc1fb0fac1bc844ed39f43
David Moody <david@davidxmoody.com>
2018-04-15 16:45:34 +0100

    Add very basic calendar view

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,56 @@
 import * as React from "react"
 import "./App.css"
+import Calendar from "./components/Calendar"
+import * as moment from "moment"
 
-export default class App extends React.Component {
+const EVENTS_URL = "http://localhost:8114/scrubbed-data/atracker/events.json"
+
+function makeDays(startDate: string, endDate?: string): string[] {
+  let day = moment(startDate)
+  const days = []
+  while (day.isBefore(moment(endDate))) {
+    days.push(day.format("YYYY-MM-DD"))
+    day = day.add(1, "day")
+  }
+  return days
+}
+
+interface State {
+  events: null | Array<{
+    name: string
+    start: number
+    end: number
+  }>
+  days: null | string[]
+}
+
+export default class App extends React.Component<{}, State> {
+  constructor(props: {}) {
+    super(props)
+    this.state = {
+      events: null,
+      days: null,
+    }
+  }
+
+  async componentDidMount() {
+    try {
+      const events = await fetch(EVENTS_URL).then(res => res.json())
+      const days = makeDays(moment(events[0].start * 1000).format("YYYY-MM-DD"))
+      this.setState({events, days})
+    } catch (e) {
+      window.alert(e)
+    }
+  }
+
   render() {
+    if (!this.state.events || !this.state.days) {
+      return <em style={{opacity: 0.5}}>Loading...</em>
+    }
+
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <Calendar days={this.state.days} events={this.state.events} />
       </div>
     )
   }
---
+++ src/components/Calendar.tsx
@@ -0,0 +1,50 @@
+import * as React from "react"
+import "./Calendar.css"
+import * as moment from "moment"
+
+interface Props {
+  days: string[]
+  events: Array<{
+    name: string
+    start: number
+    end: number
+  }>
+}
+
+export default function Calendar(props: Props) {
+  return (
+    <div className="calendar">
+      {props.days.map(day => {
+        const dayStartNum = parseFloat(moment(day).format("X"))
+        const dayEndNum = parseFloat(moment(day).add(1, "day").format("X"))
+        const eventsForDay = props.events.filter(({start}) => {
+          return start >= dayStartNum && start < dayEndNum
+        })
+
+        const eventsWithPercentages = eventsForDay.map(event => {
+          return {
+            ...event,
+            startPercentage: `${Math.max(0, (event.start - dayStartNum) / (dayEndNum - dayStartNum)) * 100}%`,
+            endPercentage: `${100 - Math.min(1, (event.end - dayStartNum) / (dayEndNum - dayStartNum)) * 100}%`,
+          }
+        })
+
+        return (
+          <div key={day} className="day">
+            {eventsWithPercentages.map(event => (
+              <div
+                className="event"
+                style={{
+                  top: event.startPercentage,
+                  bottom: event.endPercentage,
+                }}
+              >
+                {event.name}
+              </div>
+            ))}
+          </div>
+        )
+      })}
+    </div>
+  )
+}
